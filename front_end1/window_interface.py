f4a97b9dbd66f77e78326a0281bc78b0e2581530
bablu4007 <babluray0515@gmail.com>
2022-02-07 08:20:16 +0545

    image uploaded

--- front_end1/window_interface.py
+++ front_end1/window_interface.py
@@ -24,7 +24,7 @@ class Mainwindow:
         self.container_first.place(x=60, y=50, width=1380, height=780)
         self.canvas_btn1 = Canvas(self.container_first, width=150, height=100)
         self.canvas_btn1.place(x=400, y=30)
-        self.photo1 = PhotoImage(file="C:\\Users\Bablu\pythonProject\\final_assignment_semIII\\images\\book.1png.png")
+        self.photo1 = PhotoImage(file="C:\\Users\Bablu\pythonProject\\final_assignment_semIII\\images\\book.jpg")
         self.canvas_btn1.create_image(0, 0, image=self.photo1, anchor=NW)
         # =======button1=====
         self.btn1 = Button(self.container_first, text="Course", font=("times new roman", 15, "bold"), bd=8,
